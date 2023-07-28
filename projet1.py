dc2c4c494abdf9a8278bd06a5d896ecca18f2d4c
BenSerade <stephane.benserade@livecampus.tech>
2023-07-28 15:26:17 +0200

    Mise à jour

--- projet1.py
+++ projet1.py
@@ -2,10 +2,15 @@ import requests, os, pathlib, csv, json
 from typing import Any
 from Projet1.methodes import GeoApi
 
-geo_api = GeoApi()
-code_departement = input("Veuillez entrer votre département : ")
-populations_communes = geo_api.get_population_departement(code_departement)
 
-somme_population = sum(populations_communes)
-print("Populations des communes :", populations_communes)
-print("Somme de la population :", somme_population)
+def main():
+    geo_api = GeoApi()
+    code_departement = input("Veuillez entrer votre département : ")
+    populations_communes = geo_api.get_population_departement(code_departement)
+
+    somme_population = sum(populations_communes)
+    print("Populations des communes :", populations_communes)
+    print("Somme de la population :", somme_population)
+
+if __name__ == "__main__":
+    main()
